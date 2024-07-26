541c328bbad2a4e5304d0407687d22ffeb5e0de1
Ankush.upadhyay <ankush.upadhyay@squareops.com>
2024-07-26 20:45:03 +0530

    Updated module version

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -18,7 +18,7 @@ locals {
 # Using "squareops/keypair/aws" module to generate an EC2 key pair and storing it in ssm parameter store.
 module "key_pair" {
   source             = "squareops/keypair/aws"
-  version            = "1.0.2"
+  version            = "1.0.3"
   key_pair_name      = format("%s-%s-kp", local.environment, local.name)
   environment        = local.environment
   ssm_parameter_path = format("%s-%s-ssm", local.environment, local.name) #SSM parameter secret name
